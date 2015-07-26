7cc4e772c1441c01001f9731ed4150b48412cfaa
Vincenzo Ferme <fermevincenzo@gmail.com>
2015-07-26 19:00:55 +0200

    Managed exceptions

---
+++ api/src/main/java/cloud/benchflow/compose/responses/Error.java
@@ -0,0 +1,51 @@
+package cloud.benchflow.compose.responses;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Error {
+	
+	private int code;
+	private String message;
+	private String description;
+	
+	/**
+	 * @return the code
+	 */
+	@JsonProperty
+	public int getCode() {
+		return code;
+	}
+	/**
+	 * @param code the code to set
+	 */
+	public void setCode(int code) {
+		this.code = code;
+	}
+	/**
+	 * @return the message
+	 */
+	@JsonProperty
+	public String getMessage() {
+		return message;
+	}
+	/**
+	 * @param message the message to set
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	/**
+	 * @return the description
+	 */
+	@JsonProperty
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
